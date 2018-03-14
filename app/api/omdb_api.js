666a519b2bc0b1936350aafc0c37a2d9fd0de665
pjvalentini <pj.valentini@gmail.com>
2018-03-13 20:33:10 -0400

    fixes to api url

--- app/api/omdb_api.js
+++ app/api/omdb_api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 module.exports = (movie) => {
   let api_key = '1a18ddb3'
-  let api_url = `http://www.omdbapi.com/?apikey=${api_key}&t=${movie}`
+  let api_url = `https://www.omdbapi.com/?apikey=${api_key}&t=${movie}`
   return axios.get(api_url, {
 	headers: {
 		'content-type': 'application/json',
